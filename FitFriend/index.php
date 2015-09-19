a87be173e52dd799b2b9fa3e3e392f1700260cc2
Andrew Haaland <haalanda1@hawkmail.newpaltz.edu>
2015-09-19 00:05:58 +0000

    FitFriend commit 1

---
+++ FitFriend/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>FitFriend</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        </head>
+    <body>
+        <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">FitFriend <span class="glyphicon glyphicon-cutlery"></span> </a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li ><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+          <form class="navbar-form navbar-right" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username">
+            <input type="text" class="form-control" placeholder="Password">
+          </div>
+          <button type="submit" class="btn btn-success">Sign in</button>
+          <button type="button" class="btn btn-default">Sign Up</button>
+          <button type="button" class="btn btn-primary">fb oAuth sign in</button>
+           </form>
+        </div>
+      </div>
+    </nav>
+        <div class="container bg-img">
+            <div class="row">
+                <div class="col-md-6">
+                  <h1>Losing weight has never been easier than with FitFriend!</h1>
+                </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </body>
+</html>
