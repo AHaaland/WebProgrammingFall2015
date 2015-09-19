b5821e5e529d4278fa1be7e2c10df44358f86d96
Andrew Haaland <haalanda1@hawkmail.newpaltz.edu>
2015-09-19 03:02:51 +0000

    FitFriend commit 3

--- FitFriend/index.php
+++ FitFriend/index.php
@@ -9,16 +9,16 @@
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
-        </head>
+    </head>
     <body>
         <nav class="navbar navbar-default navbar-static-top">
-        <div class="container-fluid">
+        <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">FitFriend <span class="glyphicon glyphicon-cutlery"></span> </a>
         </div>
@@ -27,20 +27,49 @@
             <li ><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
           </ul>
-          <form class="navbar-form navbar-right" role="search">
-          <button type="submit" class="btn btn-success">Sign in</button>
-          <button type="button" class="btn btn-primary">fb oAuth sign in</button>
+          <form class="navbar-form navbar-right" role="buttonpanel">
+              <button type="button" class="btn btn-success">Sign in</button>
+              <button type="button" class="btn btn-primary">fb oAuth sign in</button>
            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+        </nav>
         <div class="text-center jumbotron bg-img">
             <h1>Losing weight has never been easier than with FitFriend!!</h1>
             <h4>If you want to lose weight or keep track of your food, sign up for the easiest food tracker you will find on the net!</h4>
-            <button type="submit" class="btn btn-primary center">Sign in</button>
+            <button type="button" class="btn btn-primary">Sign in</button>
             <button type="button" class="btn btn-primary">Sign Up</button>
+        </div>
+        <div class="container">
+        <div class="col-md-4 col-xs-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar convallis dolor, id ultricies lorem pellentesque eu. Morbi sapien ante, consequat vitae sagittis vitae, tincidunt ac lectus. Sed augue magna, pharetra nec lorem id, sagittis feugiat odio. Nunc fermentum, elit vel facilisis efficitur, diam massa faucibus ante, porttitor maximus tellus libero in nulla. Quisque in suscipit quam. Morbi nibh diam, congue sit amet justo vel, hendrerit suscipit elit. Sed semper enim fermentum viverra venenatis. Proin quis dictum est. Etiam ornare leo ac dolor suscipit, nec egestas nisi maximus. Cras dictum luctus lacus et maximus. Aenean pellentesque enim eget elit laoreet, eget sodales sapien tincidunt.
+            In feugiat nunc vel efficitur mollis. Nunc fringilla volutpat porta. Nam et semper diam. Mauris ut sapien feugiat, dignissim nunc sed, commodo lorem. Nam non blandit tortor. Suspendisse suscipit dapibus consequat. Ut mauris risus, gravida fringilla sem quis, faucibus rutrum dolor. Mauris id aliquam odio. Fusce sit amet ante aliquet, tincidunt velit cursus, rutrum ante. Nam a fermentum risus. Nulla volutpat vitae eros a rutrum. Pellentesque imperdiet sem interdum odio commodo, ut ultricies elit semper. Mauris porttitor enim et lorem lacinia, sit amet eleifend lacus fringilla.
+            Integer arcu ex, porta sit amet iaculis suscipit, dictum et urna. Morbi pretium mauris dolor, eget pharetra nulla dapibus id. Aliquam sed nibh at ipsum vulputate feugiat sit amet sit amet diam. Vivamus ac diam sed enim mattis efficitur. Duis quam sem, pellentesque in pulvinar eu, tristique sed nulla. Ut lectus felis, faucibus sed porta quis, luctus in velit. Aliquam nulla leo, mattis ut auctor quis, aliquet vel eros. Morbi eget turpis nec dolor auctor porta sed et felis. Aliquam sit amet libero pharetra, sodales tellus sed, rhoncus lacus. Cras porta, leo pharetra cursus ullamcorper, dui turpis maximus quam, vitae gravida odio dolor euismod tellus.
+            Curabitur imperdiet euismod erat. Vestibulum ut augue mauris. Sed vestibulum porta tincidunt. Ut at neque vulputate tellus varius pretium. Duis nisl sapien, pulvinar eu lobortis eu, commodo sit amet quam. Etiam quis mollis tellus. Duis et odio interdum, posuere lectus sit amet, consequat enim. Donec scelerisque est eu nunc dictum maximus. Suspendisse nibh quam, tempus laoreet venenatis sit amet, posuere vitae dolor. Vestibulum sed volutpat urna.
+            Etiam ultrices sem vel massa efficitur consequat. Ut commodo libero ac sollicitudin pellentesque. Ut nec mauris vel lacus euismod lobortis. Donec porta lobortis tellus nec ullamcorper. In hac habitasse platea dictumst. Sed porttitor sodales est, id vulputate sapien congue ut. Aliquam ex ligula, lobortis molestie ullamcorper eu, iaculis a magna. Sed sed augue justo.</p>
+        </div>
+        <div class="col-md-4 col-xs-5">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar convallis dolor, id ultricies lorem pellentesque eu. Morbi sapien ante, consequat vitae sagittis vitae, tincidunt ac lectus. Sed augue magna, pharetra nec lorem id, sagittis feugiat odio. Nunc fermentum, elit vel facilisis efficitur, diam massa faucibus ante, porttitor maximus tellus libero in nulla. Quisque in suscipit quam. Morbi nibh diam, congue sit amet justo vel, hendrerit suscipit elit. Sed semper enim fermentum viverra venenatis. Proin quis dictum est. Etiam ornare leo ac dolor suscipit, nec egestas nisi maximus. Cras dictum luctus lacus et maximus. Aenean pellentesque enim eget elit laoreet, eget sodales sapien tincidunt.
+            In feugiat nunc vel efficitur mollis. Nunc fringilla volutpat porta. Nam et semper diam. Mauris ut sapien feugiat, dignissim nunc sed, commodo lorem. Nam non blandit tortor. Suspendisse suscipit dapibus consequat. Ut mauris risus, gravida fringilla sem quis, faucibus rutrum dolor. Mauris id aliquam odio. Fusce sit amet ante aliquet, tincidunt velit cursus, rutrum ante. Nam a fermentum risus. Nulla volutpat vitae eros a rutrum. Pellentesque imperdiet sem interdum odio commodo, ut ultricies elit semper. Mauris porttitor enim et lorem lacinia, sit amet eleifend lacus fringilla.
+            Integer arcu ex, porta sit amet iaculis suscipit, dictum et urna. Morbi pretium mauris dolor, eget pharetra nulla dapibus id. Aliquam sed nibh at ipsum vulputate feugiat sit amet sit amet diam. Vivamus ac diam sed enim mattis efficitur. Duis quam sem, pellentesque in pulvinar eu, tristique sed nulla. Ut lectus felis, faucibus sed porta quis, luctus in velit. Aliquam nulla leo, mattis ut auctor quis, aliquet vel eros. Morbi eget turpis nec dolor auctor porta sed et felis. Aliquam sit amet libero pharetra, sodales tellus sed, rhoncus lacus. Cras porta, leo pharetra cursus ullamcorper, dui turpis maximus quam, vitae gravida odio dolor euismod tellus.
+            Curabitur imperdiet euismod erat. Vestibulum ut augue mauris. Sed vestibulum porta tincidunt. Ut at neque vulputate tellus varius pretium. Duis nisl sapien, pulvinar eu lobortis eu, commodo sit amet quam. Etiam quis mollis tellus. Duis et odio interdum, posuere lectus sit amet, consequat enim. Donec scelerisque est eu nunc dictum maximus. Suspendisse nibh quam, tempus laoreet venenatis sit amet, posuere vitae dolor. Vestibulum sed volutpat urna.
+            Etiam ultrices sem vel massa efficitur consequat. Ut commodo libero ac sollicitudin pellentesque. Ut nec mauris vel lacus euismod lobortis. Donec porta lobortis tellus nec ullamcorper. In hac habitasse platea dictumst. Sed porttitor sodales est, id vulputate sapien congue ut. Aliquam ex ligula, lobortis molestie ullamcorper eu, iaculis a magna. Sed sed augue justo.</p>
+        </div>
+        <div class="col-md-4 col-xs-3">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar convallis dolor, id ultricies lorem pellentesque eu. Morbi sapien ante, consequat vitae sagittis vitae, tincidunt ac lectus. Sed augue magna, pharetra nec lorem id, sagittis feugiat odio. Nunc fermentum, elit vel facilisis efficitur, diam massa faucibus ante, porttitor maximus tellus libero in nulla. Quisque in suscipit quam. Morbi nibh diam, congue sit amet justo vel, hendrerit suscipit elit. Sed semper enim fermentum viverra venenatis. Proin quis dictum est. Etiam ornare leo ac dolor suscipit, nec egestas nisi maximus. Cras dictum luctus lacus et maximus. Aenean pellentesque enim eget elit laoreet, eget sodales sapien tincidunt.
+            In feugiat nunc vel efficitur mollis. Nunc fringilla volutpat porta. Nam et semper diam. Mauris ut sapien feugiat, dignissim nunc sed, commodo lorem. Nam non blandit tortor. Suspendisse suscipit dapibus consequat. Ut mauris risus, gravida fringilla sem quis, faucibus rutrum dolor. Mauris id aliquam odio. Fusce sit amet ante aliquet, tincidunt velit cursus, rutrum ante. Nam a fermentum risus. Nulla volutpat vitae eros a rutrum. Pellentesque imperdiet sem interdum odio commodo, ut ultricies elit semper. Mauris porttitor enim et lorem lacinia, sit amet eleifend lacus fringilla.
+            Integer arcu ex, porta sit amet iaculis suscipit, dictum et urna. Morbi pretium mauris dolor, eget pharetra nulla dapibus id. Aliquam sed nibh at ipsum vulputate feugiat sit amet sit amet diam. Vivamus ac diam sed enim mattis efficitur. Duis quam sem, pellentesque in pulvinar eu, tristique sed nulla. Ut lectus felis, faucibus sed porta quis, luctus in velit. Aliquam nulla leo, mattis ut auctor quis, aliquet vel eros. Morbi eget turpis nec dolor auctor porta sed et felis. Aliquam sit amet libero pharetra, sodales tellus sed, rhoncus lacus. Cras porta, leo pharetra cursus ullamcorper, dui turpis maximus quam, vitae gravida odio dolor euismod tellus.
+            Curabitur imperdiet euismod erat. Vestibulum ut augue mauris. Sed vestibulum porta tincidunt. Ut at neque vulputate tellus varius pretium. Duis nisl sapien, pulvinar eu lobortis eu, commodo sit amet quam. Etiam quis mollis tellus. Duis et odio interdum, posuere lectus sit amet, consequat enim. Donec scelerisque est eu nunc dictum maximus. Suspendisse nibh quam, tempus laoreet venenatis sit amet, posuere vitae dolor. Vestibulum sed volutpat urna.
+            Etiam ultrices sem vel massa efficitur consequat. Ut commodo libero ac sollicitudin pellentesque. Ut nec mauris vel lacus euismod lobortis. Donec porta lobortis tellus nec ullamcorper. In hac habitasse platea dictumst. Sed porttitor sodales est, id vulputate sapien congue ut. Aliquam ex ligula, lobortis molestie ullamcorper eu, iaculis a magna. Sed sed augue justo.</p>
+        </div>
+        
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <footer>
+            <div class="text-center containter">
+                <h3>©2015 Andrew Haaland All rights reserved.</h3>
+            </div>
+        </footer>
     </body>
 </html>
